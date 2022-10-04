7f00f56c543a9ecd245fb5ead307354a6b9d8612
lienepurmale <96015681+lienepurmale@users.noreply.github.com>
2022-10-04 10:52:29 +0200

    Create Cashier.java

---
+++ JavaProgramming/JavaProgramming/src/chapter4/Cashier.java
@@ -0,0 +1,32 @@
+package chapter4;
+
+/*
+ * FOR LOOP:
+ * Write a cashier program that will scan a given number of items and tally the cost.
+ */
+
+import java.util.Scanner;
+
+public class Cashier {
+
+    public static void main(String args[]) {
+
+        //1. get number of items to scan
+        System.out.println("Please enter the number of items you would like to scan:");
+        Scanner scanner = new Scanner(System.in);
+        int quantity = scanner.nextInt();
+
+
+        double total = 0;
+
+        //2. Create loop to iterate through all of the items and accumulate the costs
+        for (int i = 0; i < quantity; i++) {
+            System.out.println("Enter the cost of the item");
+            double price = scanner.nextDouble();
+            total = total + price;
+        }
+        scanner.close();
+        System.out.println("Your total is $: " + total);
+
+    }
+}
