2e5febaf06e38ae01bf55650d5d2fa5fb0befb28
lienepurmale <96015681+lienepurmale@users.noreply.github.com>
2022-10-05 11:59:49 +0200

    phoneBillRedo

---
+++ JavaProgramming/JavaProgramming/src/chapter6/PhoneBill.java
@@ -0,0 +1,24 @@
+package chapter6;
+
+/*
+ * a phone bill should have an ID, a base class, a number of allotted minutes and a number of minutes used
+ * it should also be able to calculate the overage, calculate the tax, and calculate the total.
+ * And then, it should also be able to print an itemized bill.
+ * You should also include three constructors: a default one; one that accepts the ID only; one that accepts all fields
+ *
+ */
+
+import java.util.Scanner;
+
+public class PhoneBill {
+
+    Scanner scanner = new Scanner(System.in);
+
+    public static void main(String args[]) {
+
+        PhoneBill bill = new PhoneBill();
+
+        public 
+
+    }
+}
---
+++ JavaProgramming/JavaProgramming/src/chapter6/PhoneBillCalculator2.java
@@ -0,0 +1,9 @@
+package chapter6;
+
+//create a different class that instantiates the PhoneBill and prints out an itemized bill.
+
+
+public class PhoneBillCalculator2 {
+
+
+}
