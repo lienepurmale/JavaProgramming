76c48b55a7e49e8d2a33186d5abcad5be9e24553
lienepurmale <96015681+lienepurmale@users.noreply.github.com>
2022-10-05 15:15:28 +0200

    Update LotteryTicket.java

--- JavaProgramming/JavaProgramming/src/chapter7/LotteryTicket.java
+++ JavaProgramming/JavaProgramming/src/chapter7/LotteryTicket.java
@@ -1,5 +1,9 @@
 package chapter7;
 
+//ensure there is no duplicate numbers generated
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
 import java.util.Random;
 
 public class LotteryTicket {
@@ -11,6 +15,7 @@ public class LotteryTicket {
     public static void main(String args[]) {
 
         int[] ticket = generateNumbers();
+        Arrays.sort(ticket);
         printingTicket(ticket);
 
     }
@@ -21,10 +26,37 @@ public class LotteryTicket {
         Random random = new Random();   //generates random numbers
 
         for (int i = 0; i < LENGHT; i++) {
-            ticket[i] = random.nextInt(MAX_TICKET_NUMBER) + 1;
+            int randomNumber;
+
+            //Generate random number then search to make sure it does not already exist in the array.
+            //If it does, regenerate and search again
+            do {
+                randomNumber = random.nextInt(MAX_TICKET_NUMBER) + 1;
+            }
+            while (search(ticket, randomNumber));
+
+            ticket[i] = randomNumber;
         }
+
+
         return ticket;
     }
+
+    public static boolean search(int[] array, int randomNumber) {
+        //Enhanced loop
+         /* It iterates through 'array' and
+          each time assigns the current element to 'value'
+         */
+        for (int value : array) {
+            if (value == randomNumber) {
+                return true;
+            }
+        }       /*If we've reached this point, then the entire array was searched
+          and the value was not found
+         */
+        return false;
+    }
+
 
     public static void printingTicket(int ticket[]) {
         for (int i = 0; i < LENGHT; i++) {
@@ -33,4 +65,3 @@ public class LotteryTicket {
 
     }
 }
-
