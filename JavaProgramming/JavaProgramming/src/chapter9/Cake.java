40410e20d57938eeeaf96c373c01f0201c67a29f
lienepurmale <96015681+lienepurmale@users.noreply.github.com>
2022-10-17 16:43:09 +0200

    Update Cake.java

--- JavaProgramming/JavaProgramming/src/chapter9/Cake.java
+++ JavaProgramming/JavaProgramming/src/chapter9/Cake.java
@@ -1,5 +1,13 @@
 package chapter9;
 
+/*
+The first one is going to be a superclass called Cake and it will have two fields, flavor and price.
+Next create a BirthdayCake class which will inherit from the Cake class and it has a field of its own called candles.
+And then create a third class called WeddingCake which also inherits from Cake and has a field called tiers.
+And then finally, a class called TasteTester which will test out your inheritance.
+
+ */
+
 public class Cake {
 
     public String flavor;
