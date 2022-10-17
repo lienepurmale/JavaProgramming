b4d06b12eece05ca88fa7ada8574b3979e17f5f4
lienepurmale <96015681+lienepurmale@users.noreply.github.com>
2022-10-17 15:33:00 +0200

    Update

--- JavaProgramming/JavaProgramming/src/chapter9/Employee.java
+++ JavaProgramming/JavaProgramming/src/chapter9/Employee.java
@@ -5,6 +5,11 @@ public class Employee extends Person{
     private String department;
     private int employeeId;
 
+    public Employee(){
+       super("Liene"); // Explicit call 
+        System.out.println("In Employee default constructor");
+    }
+
     public String getDepartment() {
         return department;
     }
--- JavaProgramming/JavaProgramming/src/chapter9/InheritanceTester.java
+++ JavaProgramming/JavaProgramming/src/chapter9/InheritanceTester.java
@@ -2,11 +2,10 @@ package chapter9;
 
 public class InheritanceTester {
 
-    public static void main(String args[]){
+    public static void main(String args[]) {
 
-        Person person = new Person();
         Employee employee = new Employee();
 
-        
+
     }
 }
