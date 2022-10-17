2138aebfcbb44b0706383921e0bd29cabc95c923
lienepurmale <96015681+lienepurmale@users.noreply.github.com>
2022-10-17 15:41:45 +0200

    Update Rectangle.java

--- JavaProgramming/JavaProgramming/src/chapter9/Rectangle.java
+++ JavaProgramming/JavaProgramming/src/chapter9/Rectangle.java
@@ -1,4 +1,36 @@
 package chapter9;
 
 public class Rectangle {
+
+    protected double width;
+    protected double lenght;
+    protected double sides = 4;
+
+    public double getWidth() {
+        return width;
+    }
+
+    public void setWidth(double width) {
+        this.width = width;
+    }
+
+    public double getLenght() {
+        return lenght;
+    }
+
+    public void setLenght(double lenght) {
+        this.lenght = lenght;
+    }
+
+    public double getSides() {
+        return sides;
+    }
+
+    public void setSides(double sides) {
+        this.sides = sides;
+    }
+
+    public double calculatePerimeter(){
+        return (2 * lenght) + (2* width );
+    }
 }
