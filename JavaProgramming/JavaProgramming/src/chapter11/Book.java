b6d9e4ae9abc44381eadde5fb9c74b8fd09f9113
lienepurmale <96015681+lienepurmale@users.noreply.github.com>
2022-10-18 17:00:58 +0200

    Interfaces

---
+++ JavaProgramming/JavaProgramming/src/chapter11/Book.java
@@ -0,0 +1,56 @@
+package chapter11;
+
+public class Book implements Product{
+
+    public double price;
+    public String name;
+    public String author;
+    public int pages;
+    public String color;
+
+
+    @Override
+    public double getPrice() {
+        return price;
+    }
+
+    @Override
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public int getPages() {
+        return pages;
+    }
+
+    public void setPages(int pages) {
+        this.pages = pages;
+    }
+
+    @Override
+    public String getColor() {
+        return color;
+    }
+
+    @Override
+    public void setColor(String color) {
+        this.color = color;
+    }
+}
---
+++ JavaProgramming/JavaProgramming/src/chapter11/Customer.java
@@ -0,0 +1,12 @@
+package chapter11;
+
+public class Customer {
+
+    public static void main(String args[]){
+
+        Product book = new Book();
+        book.setPrice(23.55);
+        book.setName("Animal farm");
+        book.setColor("White");
+    }
+}
