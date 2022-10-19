7af6e3f3a7303ea0830215fa5785f260212c3294
lienepurmale <96015681+lienepurmale@users.noreply.github.com>
2022-10-19 13:30:35 +0200

    Update ExceptionHandling.java

--- JavaProgramming/JavaProgramming/src/chapter13/ExceptionHandling.java
+++ JavaProgramming/JavaProgramming/src/chapter13/ExceptionHandling.java
@@ -1,27 +1,46 @@
 package chapter13;
 
-import javax.imageio.IIOException;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
 public class ExceptionHandling {
 
-    public static void main(String arg[]){
-
+    public static void main(String arg[]) {
 
         createNewFile();
+        numbersException();
 
     }
-    public static void createNewFile(){
+
+    public static void createNewFile() {
         File file = new File("resources/nonexistent.txt");
-        try{
+        try {
             file.createNewFile();
-        } catch (IOException e){
+        } catch (IOException e) {
             System.out.println("Path does not exist.");
             e.printStackTrace();
 
         }
     }
 
+    public static void numbersException() {
+        File file = new File("resources/numbers.txt");
+        Scanner fileReader = null;
+        try {
+            fileReader = new Scanner(file);
+            while (fileReader.hasNext()) {
+                double number = fileReader.nextDouble();
+                System.out.println(number);
+            }
+        } catch (FileNotFoundException | InputMismatchException e) {
+            e.printStackTrace();
+        } finally {
+            fileReader.close();
+        }
+    }
 }
+
 
