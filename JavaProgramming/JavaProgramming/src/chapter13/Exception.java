381e1a39cddce2f141dadf28946bf3aecd82ce26
lienepurmale <96015681+lienepurmale@users.noreply.github.com>
2022-10-19 13:41:30 +0200

    Update Exception.java

--- JavaProgramming/JavaProgramming/src/chapter13/Exception.java
+++ JavaProgramming/JavaProgramming/src/chapter13/Exception.java
@@ -25,7 +25,7 @@ public class Exception  {
     public static void createNewFileRethrow() throws IOException {
         File file = new File("resources/nonexistent.txt");
         file.createNewFile();
-        
+
     }
 
 
