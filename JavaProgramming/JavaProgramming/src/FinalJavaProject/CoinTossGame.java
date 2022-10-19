10e61f22a2eaaa086e9cbd897b8fb2471c59b9c1
lienepurmale <96015681+lienepurmale@users.noreply.github.com>
2022-10-19 16:15:08 +0200

    Create CoinTossGame.java

---
+++ JavaProgramming/JavaProgramming/src/FinalJavaProject/CoinTossGame.java
@@ -0,0 +1,22 @@
+package FinalJavaProject;
+
+import java.util.Scanner;
+
+public class CoinTossGame {
+
+    public Scanner scanner;
+
+    public static void main(String args[]) {
+
+        CoinTossGame game = new CoinTossGame();
+        game.scanner = new Scanner(System.in);
+
+        playerOne();
+        playerTwo();
+    }
+
+    public static void askName(String name) {
+       
+    }
+
+}
